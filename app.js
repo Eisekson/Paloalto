168f27ca9de3fe29a659f2fbe5edb90c32efd3c9
Eisekson <ksa541@gmail.com>
2014-05-25 21:24:23 +0800

    update

--- app.js
+++ app.js
@@ -62,19 +62,83 @@ app.get('/hello', function (req, res) {
 
     res.jsonp(data);
 });
-app.get('/h1/:id',function(req,res){
-    
-    res.render('h1/'+req.params.id,{});
+app.get('/h1/:id', function (req, res) {
+
+    res.render('h1/' + req.params.id, {});
 })
 
-function rendertourl(req,res){
+function rendertourl(req, res) {
     var url = req.url;
     res.render(url.substr(1, url.length - 1), {});
 }
 
 app.post('/query/traffic', showchart.queryTraffic);
 app.post('/query/apptoip', showchart.apptoip);
+app.post('/editpolicyrule', function (req, res) {
 
+    console.log(req.body);
+    var u = req.body;
+    var mysql = require('mysql');
+    var sql = "INSERT INTO policyrule VALUES (null," +
+        "'"+ u.target+"'," +
+        "'"+ u.flow+"'," +
+        "'"+ u.session+"'," +
+        "'"+ u.application+"'," +
+        "'"+ u.violate+"'," +
+        "'"+ u.thread+"'," +
+        "'"+ u.beginTime+"'," +
+        "'"+ u.timeCycle+"'," +
+        "'"+ u.enable+"'," +
+        "'"+ u.reason+"'," +
+        "'"+ u.action+
+        "')";
+    console.log(sql);
+    /*
+     * appIndex  0 前十大 1 前二十大 2 前三十大 3 前四十大
+     */
+    var connection = mysql.createConnection({
+        host: '120.110.114.25',
+        user: 'Paloalto',
+        password: 'password',
+        database: 'paloalto'
+    });
+    connection.connect();
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            //throw err;
+            res.jsonp('error');
+        }
+        console.log(result);
+        res.jsonp('success');
+
+    });
+    connection.end();
+});
+app.post('/getpolicyrule', function (req, res) {
+    var mysql = require('mysql');
+    var sql = "select * from policyrule";
+    var connection = mysql.createConnection({
+        host: '120.110.114.25',
+        user: 'Paloalto',
+        password: 'password',
+        database: 'paloalto'
+    });
+    connection.connect();
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(error);
+            //throw err;
+            res.jsonp('error');
+        }
+        console.log(result);
+
+        res.jsonp(result);
+
+    });
+    connection.end();
+})
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
